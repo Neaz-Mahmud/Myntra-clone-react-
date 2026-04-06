c761fa903a26940f71deb8655057dd4994bc9946
Neaz <isti@gmail.com>
2026-04-06 08:45:49 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { useState } from 'react'
+
+createRoot(document.getElementById('root')).render(
+   cnonst [a,b]= useState(5);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
